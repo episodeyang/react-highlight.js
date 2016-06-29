eb8382dd888632d5b5c5678b98db2f736700cd76
Ge Yang <yangge1987@gmail.com>
2016-06-29 00:44:22 -0700

    use eslint instead

--- src/Highlight.js
+++ src/Highlight.js
@@ -1,36 +1,36 @@
-import React, { Component, PropTypes } from 'react'
-import { findDOMNode } from 'react-dom'
+import React, {Component, PropTypes} from 'react'
+import {findDOMNode} from 'react-dom'
 import highlight from 'highlight.js'
 
 var {node, string} = PropTypes;
 export default class Highlight extends Component {
 
-  static propTypes = {
-    children: node,
-    language: string
-  };
+    static propTypes = {
+        children: node,
+        language: string
+    };
 
-  componentDidMount () {
-    highlight.highlightBlock(findDOMNode(this.refs.code))
-  }
+    componentDidMount() {
+        highlight.highlightBlock(findDOMNode(this.refs.code))
+    }
 
-  componentDidUpdate () {
-    highlight.initHighlighting.called = false;
-    highlight.highlightBlock(findDOMNode(this.refs.code))
-  }
+    componentDidUpdate() {
+        highlight.initHighlighting.called = false;
+        highlight.highlightBlock(findDOMNode(this.refs.code))
+    }
 
-  render () {
-    const { children, language } = this.props;
+    render() {
+        const {children, language} = this.props;
 
-    return (
-      <pre {...this.props}>
+        return (
+            <pre {...this.props}>
         <code
-          className={language}
-          ref='code'
+            className={language}
+            ref='code'
         >
           {children}
         </code>
       </pre>
-    )
-  }
+        )
+    }
 }
